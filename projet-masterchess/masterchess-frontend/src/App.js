add6e362ed5c715b627fcc4ff2e6ad62b014252b
gabi <3122683+gabium@users.noreply.github.com>
2025-02-15 18:32:56 -0500

    format de page principale - code a reutiliser et ameliorer

--- projet-masterchess/masterchess-frontend/src/App.js
+++ projet-masterchess/masterchess-frontend/src/App.js
@@ -1,23 +1,75 @@
-import logo from './logo.svg';
+import logo from './style/logo.svg';
+import loginicon from "./style/login-icon.svg";
+import learnicon from "./style/learn-icon.svg";
+import buyicon from "./style/buy-icon.svg";
+import gemicon from "./style/gem-icon.svg";
+import gemaddicon from "./style/gem-add-icon.svg";
+import rectangle from "./style/rectangle.svg";
+
+import Jeu from "./jeu/Jeu.js";
+
+//import outfitmedium from "./style/Outfit-Medium.ttf";
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="container">
+      <div class="my-sidebar">
+        <div class="my-sidebar-header">
+          <div class="my-logo">
+            <img src={logo} />
+          </div>
+          <div class="my-navbar">
+            <div class="my-navbaroptions">
+              <div class="my-navoption">
+                <img class="my-icon" src={loginicon} />
+                <label class="my-optionlabel">Login</label>
+              </div>
+              <div class="my-navoption">
+                <img class="my-icon" src={learnicon} />
+                <label class="my-optionlabel">Learn</label>
+              </div>
+              <div class="my-navoption">
+                <img class="my-icon" src={buyicon} />
+                <label class="my-optionlabel">Buy</label>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="my-sidebar-footer">
+          <div class="my-gemcounter">
+            <div class="my-gemindicator">
+              <img class="my-gemicon" src={gemicon} />
+              <label class="my-gemcountlabel">300</label>
+            </div>
+            <img class="my-gemaddbutton" src={gemaddicon} />
+          </div>
+          <div class="my-sidebar-footer-userparent">
+            <div class="my-sidebar-footer-user">
+              <div class="my-sidebar-footer-userpfp">
+                <img class="my-gemicon" src={rectangle} />
+              </div>
+              <div class="my-sidebar-footer-userdata">
+                <label class="my-sidebar-footer-username">Test</label>
+                <label class="my-sidebar-footer-informations">Informations</label>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="content">
+        {/* <Jeu></Jeu> */}
+      </div>
+      <div class="my-sidebar">
+        <div class="move-info-panel">
+          <br />
+          <label class="move-info-label">A2A3</label>
+          <label class="move-info-label">B2B3</label>
+          <label class="move-info-label">C2C3</label>
+          <label class="move-info-label">D2D3</label>
+          <label class="move-info-label">E2E3</label>
+        </div>
+      </div>
     </div>
   );
 }
