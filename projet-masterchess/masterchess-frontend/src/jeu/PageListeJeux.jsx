798a348d300807a569da8346db75f4507597cdce
gabi <3122683+gabium@users.noreply.github.com>
2025-05-17 23:42:01 -0400

    refait pagelistejeux

--- projet-masterchess/masterchess-frontend/src/jeu/PageListeJeux.jsx
+++ projet-masterchess/masterchess-frontend/src/jeu/PageListeJeux.jsx
@@ -189,83 +189,101 @@ function PageListeJeux() {
         <div className="parties-container">
             { disponnibleChercher && 
             <div className="panneau-parties-header card">
-                <div className="card-body">
-                    <h5 class="card-title">Trouver une partie</h5>
-                    <div className="input-group mw-100 p-1">
-                        <div className="input-group-prepend w-15">
-                            <span className="input-group-text" id="basic-addon1">Joueur 1</span>
+                <div className="card-body card-body-obtenir-partie">
+                    <h5 class="card-title">Jouer</h5>
+                    <ul class="nav nav-tabs" id="myTab" role="tablist">
+                        <li class="nav-item" role="presentation">
+                            <button class="nav-link active" id="creer-tab" data-bs-toggle="tab" data-bs-target="#creer" type="button" role="tab" aria-controls="creer" aria-selected="true">Créer</button>
+                        </li>
+                        <li class="nav-item" role="presentation">
+                            <button class="nav-link" id="liste-tab" data-bs-toggle="tab" data-bs-target="#liste" type="button" role="tab" aria-controls="liste" aria-selected="false">Liste</button>
+                        </li>
+                        <li class="nav-item" role="presentation">
+                            <button class="nav-link" id="trouver-tab" data-bs-toggle="tab" data-bs-target="#trouver" type="button" role="tab" aria-controls="trouver" aria-selected="false">Trouver</button>
+                        </li>
+                    </ul>
+                    <div class="tab-content" id="myTabContent">
+                        <div class="tab-pane fade show active" id="creer" role="tabpanel" aria-labelledby="home-tab">
+                            <div className="input-group mw-100 p-1">
+                                <div className="input-group-prepend w-15">
+                                    <span className="input-group-text" id="basic-addon1">Joueur 1</span>
+                                </div>
+                                <input id="nomprofiljeu1Creer" type="text" className="form-control w-50" placeholder="Nom d'utilsateur du joueur 1" aria-label="Nom d'utilsateur du joueur 1" aria-describedby="basic-addon1"></input>
+                                <div id="nomprofiljeu1Creer-invalid" className="invalid-tooltip"></div>
+                            </div>
+                            <br></br>
+                            <div className="input-group mw-100 p-1">
+                                <div className="input-group-prepend w-15">
+                                    <span className="input-group-text" id="basic-addon2">Joueur 2</span>
+                                </div>
+                                <input id="nomprofiljeu2Creer" type="text" className="form-control w-50" placeholder="Nom d'utilsateur du joueur 2" aria-label="Nom d'utilsateur du joueur 2" aria-describedby="basic-addon2"></input>
+                                <div id="nomprofiljeu2Creer-invalid" className="invalid-tooltip"></div>
+                            </div>
+                            <br></br>
+                            <button id="btnCreer" className="buttonCreate" onClick={onBtnCreer}>Créer partie</button>
                         </div>
-                        <input id="nomprofiljeu1Creer" type="text" className="form-control w-50" placeholder="Nom d'utilsateur du joueur 1" aria-label="Nom d'utilsateur du joueur 1" aria-describedby="basic-addon1"></input>
-                        <div id="nomprofiljeu1Creer-invalid" className="invalid-tooltip"></div>
-                    </div>
-                    <br></br>
-                    <div className="input-group mw-100 p-1">
-                        <div className="input-group-prepend w-15">
-                            <span className="input-group-text" id="basic-addon2">Joueur 2</span>
+                        <div class="tab-pane fade" id="liste" role="tabpanel" aria-labelledby="liste-tab">
+                            <div id="browser-parties" className="browser-parties card-body overflow-scroll">
+                                <div className="input-group mw-100 p-2">
+                                    {/* <label>
+                                        <input id="cbMesParties"
+                                            label="Mes parties"
+                                            onChange={onMesPartiesClicked}
+                                            key="mesPartiesKey"
+                                            type="checkbox"
+                                            checked={mesParties}
+                                        />
+                                        Mes parties
+                                    </label> */}
+                                    {/* <br></br> */}
+                                    <DisplayPartiesServiceContext.Provider value={ { partiesEncours, setPartiesEncours, displayPartiesService } }>
+                                    <table className="table-parties table table-hover table-dark mw-100">
+                                        <colgroup>
+                                            <col span="1" style={ { width: "10%" } }/>
+                                            <col span="1" style={ { width: "40%" } }/>
+                                            <col span="1" style={ { width: "40%" } }/>
+                                        </colgroup>
+                                        <thead>
+                                            <tr>
+                                                <th scope="col">#</th>
+                                                <th scope="col">Joueur 1</th>
+                                                <th scope="col">Joueur 2</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            <>
+                                                {Object.values(partiesEncours ?? []).map((entry, i) =>
+                                                {
+                                                    // if(mesParties)
+                                                        if(!(entry.id_joueur1 === sessionUsager?.id_profiljeu || entry.id_joueur2 === sessionUsager?.id_profiljeu))
+                                                            return <></>;
+                                                    // <DisplayPartieComponent ref={partiesEndRef} key={entry.id} partie={entry} id={"display-partie" + entry.id} onClick={() => onBtnOuvrirPartie(entry.id, sessionUsager) }></DisplayPartieComponent>)}
+                                                    const trcomp = (<tr id={"display-partie" + entry.id} onClick={() => onBtnOuvrirPartie(entry.id, sessionUsager)} key={entry.id} ref={partiesEndRef}>
+                                                            <th scope="row">{ entry?.id }</th>
+                                                            <td>{ entry?.compte_joueur1 }</td>
+                                                            <td>{ entry?.compte_joueur2 }</td>
+                                                        </tr>);
+                                                    if(i == partiesEncours.length - 1)
+                                                    {
+                                                        partiesEnded();
+                                                    }
+                                                    return trcomp;
+                                                }
+                                                )}
+                                            </>
+                                        </tbody>
+                                    </table>
+                                    </DisplayPartiesServiceContext.Provider>
+                                </div>
+                            </div>
+                            <button id="btnRafraichirParties" className="buttonRefresh" onClick={onBtnRefreshPartiesEncours}>Rafraichir</button>
                         </div>
-                        <input id="nomprofiljeu2Creer" type="text" className="form-control w-50" placeholder="Nom d'utilsateur du joueur 2" aria-label="Nom d'utilsateur du joueur 2" aria-describedby="basic-addon2"></input>
-                        <div id="nomprofiljeu2Creer-invalid" className="invalid-tooltip"></div>
-                    </div>
-                    <br></br>
-                    <div className="input-group mw-100 p-1">
-                        <button id="btnCreer" className="buttonCreate" onClick={onBtnCreer}>Créer partie</button>
-                        <button id="btnRafraichirParties" className="buttonRefresh" onClick={onBtnRefreshPartiesEncours}>Rafraichir</button>
-                        <button id="btnChercherPartie" className="buttonChercher" onClick={onBtnChercherPartie}>Trouver</button>
-                        { rechercheEncours == 1 && <label id="lblChercherPartie">recherche en cours</label> }
-                    </div>
-                </div>
-                <div id="browser-parties" className="browser-parties card-body overflow-scroll">
-                    <div className="input-group mw-100 p-2">
-                        {/* <label>
-                            <input id="cbMesParties"
-                                label="Mes parties"
-                                onChange={onMesPartiesClicked}
-                                key="mesPartiesKey"
-                                type="checkbox"
-                                checked={mesParties}
-                            />
-                            Mes parties
-                        </label> */}
-                        {/* <br></br> */}
-                        <DisplayPartiesServiceContext.Provider value={ { partiesEncours, setPartiesEncours, displayPartiesService } }>
-                        <table className="table-parties table table-hover table-dark mw-100">
-                            <colgroup>
-                                <col span="1" style={ { width: "10%" } }/>
-                                <col span="1" style={ { width: "40%" } }/>
-                                <col span="1" style={ { width: "40%" } }/>
-                            </colgroup>
-                            <thead>
-                                <tr>
-                                    <th scope="col">#</th>
-                                    <th scope="col">Joueur 1</th>
-                                    <th scope="col">Joueur 2</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <>
-                                    {Object.values(partiesEncours ?? []).map((entry, i) =>
-                                    {
-                                        // if(mesParties)
-                                            if(!(entry.id_joueur1 === sessionUsager?.id_profiljeu || entry.id_joueur2 === sessionUsager?.id_profiljeu))
-                                                return <></>;
-                                        // <DisplayPartieComponent ref={partiesEndRef} key={entry.id} partie={entry} id={"display-partie" + entry.id} onClick={() => onBtnOuvrirPartie(entry.id, sessionUsager) }></DisplayPartieComponent>)}
-                                        const trcomp = (<tr id={"display-partie" + entry.id} onClick={() => onBtnOuvrirPartie(entry.id, sessionUsager)} key={entry.id} ref={partiesEndRef}>
-                                                <th scope="row">{ entry?.id }</th>
-                                                <td>{ entry?.compte_joueur1 }</td>
-                                                <td>{ entry?.compte_joueur2 }</td>
-                                            </tr>);
-                                        if(i == partiesEncours.length - 1)
-                                        {
-                                            partiesEnded();
-                                        }
-                                        return trcomp;
-                                    }
-                                    )}
-                                    {/* <tr id="bottom-parties" className="liste-parties-soak" ref={partiesEndRef}></tr> */}
-                                </>
-                            </tbody>
-                        </table>
-                        </DisplayPartiesServiceContext.Provider>
+                        <div class="tab-pane fade" id="trouver" role="tabpanel" aria-labelledby="trouver-tab">
+                            <div className="input-group mw-100 p-1">
+                                <button id="btnChercherPartie" className="buttonChercher" onClick={onBtnChercherPartie}>Trouver</button>
+                                { rechercheEncours == 1 && <label id="lblChercherPartie">recherche en cours</label> }
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
